493ea50523f8611e3f093ec53c34899b1f244919
UrosHCS <uroshcs@gmail.com>
2019-06-16 13:32:45 +0200

    sprint 1 done

--- graph.js
+++ graph.js
@@ -14,8 +14,60 @@ const firstStep = (() => {
     }
 })()
 
-const currentStep = firstStep;
+if (!firstStep) {
+    console.error('It would be nice if we had a FIRST step')
+}
+
+const findExitingMoves = (stepName) => {
+    const foundMoves = []
+    for (move of data.moves) {
+        if (move.from.name === stepName) {
+            foundMoves.push(move)
+        }
+    }
+
+    return foundMoves
+}
+
+const findTo = (toName) => {
+    for (step of data.steps) {
+        if (step.name === toName) {
+            return step
+        }
+    }
+}
+
+let currentStep = firstStep;
+let currentPlace = null;
+let currentTransition = null;
+let currentX = xStart
+let currentY = yStart
 
 while (true) {
-    // I'm bored
+    console.log('while')
+    currentPlace = shape.newPlace(currentX, currentY, currentStep.name)
+    currentX = currentPlace.coordinates.x + 250
+    // currentY = currentPlace.coordinates.y
+
+    if (currentTransition) {
+        shape.newArrow(currentTransition.rightEdge, currentPlace.leftEdge)
+    }
+
+    let exitingMoves = findExitingMoves(currentStep.name)
+
+    currentStep = null
+    for (exitingMove of exitingMoves) {
+         if (exitingMove.isBackward) {
+             // TODO: render backward moves and steps
+         } else {
+            currentTransition = shape.newTransition(currentPlace.rightEdge.x + 30, currentPlace.rightEdge.y, exitingMove.name)
+            shape.newArrow(currentPlace.rightEdge, currentTransition.leftEdge)
+
+            currentStep = findTo(exitingMove.to.name)
+         }
+    }
+
+    if (!currentStep) {
+        break;
+    }
 }
--- shape.js
+++ shape.js
@@ -1,13 +1,13 @@
 const c = document.getElementById("wf")
 const ctx = c.getContext("2d")
 
-const fontSize = 14
+const fontSize = 12
 const verticalTextAllignment = 5
 const arcRadius = 50
 const rectWidth = 100
 const rectHeight = 50
 const _180 = 2 * Math.PI
-const arrowHeadLength = 10 // in pixels
+const arrowHeadLength = 7 // in pixels
 
 ctx.font = fontSize + "px Arial MS"
 ctx.textAlign = "center"
@@ -39,19 +39,19 @@ const shape = {
             name: name,
             coordinates: {x: x + rectWidth / 2, y},
             leftEdge: {x: x, y: y},
-            rightEdge: {x: x, y: y},
+            rightEdge: {x: x + rectWidth, y: y},
         }
     },
 
     newArrow(from, to) {
-        // const len = arrowHeadLength;   
+        const len = arrowHeadLength;   
         ctx.beginPath();
-        // const angle = Math.atan2(to.y - from.y,to.x - from.x);
+        const angle = Math.atan2(to.y - from.y,to.x - from.x);
         ctx.moveTo(from.x, from.y);
         ctx.lineTo(to.x, to.y);
-        // ctx.lineTo(to.x - len * Math.cos(angle - Math.PI/6), to.y - len * Math.sin(angle - Math.PI/6));
+        ctx.lineTo(to.x - len * Math.cos(angle - Math.PI/6), to.y - len * Math.sin(angle - Math.PI/6));
         ctx.moveTo(to.x, to.y);
-        // ctx.lineTo(to.x - len * Math.cos(angle + Math.PI/6), to.y - len * Math.sin(angle + Math.PI/6));
+        ctx.lineTo(to.x - len * Math.cos(angle + Math.PI/6), to.y - len * Math.sin(angle + Math.PI/6));
         ctx.stroke();
     }
 }
